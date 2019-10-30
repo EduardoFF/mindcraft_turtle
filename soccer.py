64f3a78bef37e60c15c242acc6c693287c635a00
Eduardo Marcelo Feo Flushing <efeoflus@qatar.win.cmu.edu>
2019-10-30 08:56:41 +0300

    added soccer solution

--- soccer.py
+++ soccer.py
@@ -8,6 +8,16 @@ place_square(100,300, "red", label="1")
 place_square(400,500, "red", label="4")
 place_square(400,300, "red", label="3")
 
+id = scan_for_squares(180)
+if id == False:
+	# cant find
+	say("Error, can't find any square")
+else:
+	go_to_square_by_id(id)
+	align_with_left_post()
+	angle=acos(300/360.55)
+	rotate(angle/2)
+	move_forward(316)
 
 
 
